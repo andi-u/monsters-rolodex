670b5cd257f2f354c7a80be9252c9434fef28bca
Andi <andi_unguroiu@yahoo.com>
2020-02-02 16:18:53 +0200

    Updates

---
+++ precache-manifest.9ec4b8fc064f67370412154ff33e2545.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d514769da9116409b2fa50af95fbbe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0fbebce6f895d464128f",
+    "url": "/monsters-rolodex/static/css/main.13327365.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "0fbebce6f895d464128f",
+    "url": "/monsters-rolodex/static/js/main.d584ba6b.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
